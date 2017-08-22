65044c654432f000f6efaf6cd2411942a196dc51
Luca Cinquini <cinquini@LMC-038261.local>
2017-08-22 07:35:08 -0600

    Using Root service class to allow dotted notation.

--- python/workflow_manager_proxy.py
+++ python/workflow_manager_proxy.py
@@ -5,11 +5,15 @@ and sends them to a RabbitMQ server instead,
 for later consumption by RabbitMQ/OODT clients.
 '''
 
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer, list_public_methods
 import logging
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
+
+# class used to enable "workflowmgr.handleEvent" notation
+class ServiceRoot:
+    pass
 
 # main server class
 class WorkflowManagerProxy():
@@ -27,7 +31,11 @@ class WorkflowManagerProxy():
 if __name__ == "__main__":
 
     server = SimpleXMLRPCServer(('localhost', 9001), logRequests=True, allow_none=True)
-    server.register_instance(WorkflowManagerProxy())
+    server.register_introspection_functions()
+
+    root = ServiceRoot()
+    root.workflowmgr = WorkflowManagerProxy()
+    server.register_instance(root, allow_dotted_names=True)
 
     try:
         print 'Use Control-C to exit'
